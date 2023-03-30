25d36ca676cf1fd68d4cd04d14a5511a709ad65c
AswinKaruppiah <aswinthkaruppiah@gmail.com>
2023-03-31 00:46:13 +0530

    aswin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import Tech from "./Ui/Events/Tech/Tech";
 import Nontech from "./Ui/Events/Non-tech/Non-tech";
 import Workshop from "./Ui/Events/Workshop/Workshop";
-import Slider from "./Ui/Slider/Slider";
+
 import Clientside from "./Ui/Slider/Clientside";
 import Fullvideo from "./Ui/ShowFullVideo/Fullvideo";
 import Footer from "./Ui/Footer/Footer";
