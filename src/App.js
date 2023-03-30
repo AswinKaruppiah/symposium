1c1a4be9a31dcfe717be24ae9c266d7f3469ca6c
AswinKaruppiah <aswinthkaruppiah@gmail.com>
2023-03-30 23:15:55 +0530

    aswin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import About from "./Ui/About/About";
 import Events from "./Ui/Events/Events";
 import Intro from "./Ui/Intro/Intro";
 import Timer from "./Ui/Timer/Timer";
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import Tech from "./Ui/Events/Tech/Tech";
 import Nontech from "./Ui/Events/Non-tech/Non-tech";
 import Workshop from "./Ui/Events/Workshop/Workshop";
@@ -14,25 +14,26 @@ import Footer from "./Ui/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Intro />
-              <Timer />
-              <About />
-              <Events />
-              <Clientside />
-              <Fullvideo />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="/tech" element={<Tech />} />
-        <Route path="/workshop" element={<Workshop />} />
-        <Route path="/nontech" element={<Nontech />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/">
+          <Intro />
+          <Timer />
+          <About />
+          <Events />
+          <Clientside />
+          <Fullvideo />
+          <Footer />
+        </Route>
+        <Route path="/tech">
+          <Tech />
+        </Route>
+        <Route path="/workshop">
+          <Workshop />
+        </Route>
+        <Route path="/nontech">
+          <Nontech />
+        </Route>
+      </Switch>
     </div>
   );
 }
